19d53dc79c7770066eb47b18069b2b2539b90e59
Avinash Petla <avins9t@gmail.com>
2022-01-01 19:11:56 +0530

    Updated Design and included Typed.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener('click',e=>{
+    // @ts-ignore
     const isDropdown = e.target.matches("[yearButton]");
+    // @ts-ignore
     if(!isDropdown && e.target.closest("[year-dropdown]")!=null) return;
 
     let currentDropdown;
     if(isDropdown){
+        // @ts-ignore
         currentDropdown = e.target.closest("[year-dropdown]");
         currentDropdown.classList.toggle("active");
     }
@@ -15,6 +18,9 @@ document.addEventListener('click',e=>{
 })
 
 const avpIcon = document.querySelector(".hiddenStickyNameOnHover img");
+// @ts-ignore
 avpIcon.addEventListener("click",e=>{
     window.open("./docs/AvinashPetla.pdf", '_blank');
 })
+
+
